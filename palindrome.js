8b2f48695bb7bb38f0473ee2e74996543dff3e41
kavita <kavitapandey020994@gmail.com>
2023-07-07 13:04:02 +0100

    add palindrome

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+var isPalindrome = function(x) {
+    if (x < 0 || x % 10 == 0 && x !=0)  {
+        return false;
+    }
+    let t = 0;
+    while (x > t) {
+        t = t * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    return t==x || x==Math.floor(t/10);
+};
